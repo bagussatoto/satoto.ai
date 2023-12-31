1c9c0863ddfa47d48b75bb53423a4389c4ac78bd
bagussatoto <bagusbudisatoto2001@gmail,com>
2023-12-31 11:06:58 +0700

    main

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
     content: [
-      "./index.html",
+      "./index.jsx",
       "./src/**/*.{js,ts,jsx,tsx}",
     ],
     theme: {
